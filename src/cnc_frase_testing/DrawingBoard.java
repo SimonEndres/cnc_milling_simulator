547832c9ae8c8e3c52a71e4b07510edf9abff907
I516660 <I516660@WDFN34686472A.emea.global.corp.sap>
2020-05-29 14:10:42 +0200

    [Tim] Grundstruktur

---
+++ src/cnc_frase_testing/DrawingBoard.java
@@ -0,0 +1,10 @@
+package cnc_frase_testing;
+import javafx.stage.Stage;
+
+final class DrawingBoard {
+	
+	DrawingBoard(Stage primaryStage) {
+		 primaryStage.setTitle("My First JavaFX App");
+		 primaryStage.show();
+	}
+}
---
+++ src/cnc_frase_testing/Main.java
@@ -0,0 +1,15 @@
+package cnc_frase_testing;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    	new DrawingBoard(primaryStage);
+    }
+
+	public static void main(String[] args) {
+	    Application.launch(args);
+	}
+}
