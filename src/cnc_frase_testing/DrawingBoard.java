d8f9ae068a7ccf92098f9edc44bdaa633bffa706
I517002 <I517002@WDFN34686493A.emea.global.corp.sap>
2020-06-10 15:04:58 +0200

    [jonas]

--- src/cnc_frase_testing/DrawingBoard.java
+++ src/cnc_frase_testing/DrawingBoard.java
@@ -1,61 +1,43 @@
 package cnc_frase_testing;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
 
 final class DrawingBoard {
 	
+	final private Stage primaryStage;
+	final private Group root;
+	final private Scene scene;
+//	private Bohrer b1;
+	
 	DrawingBoard(Stage primaryStage) {
-		 // Create the Canvas with a width of 400 px and a height of 200 px.
-        Canvas canvas = new Canvas(800, 800);
-        // Get the graphics context of the canvas
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        // Set line width
-        gc.setLineWidth(2.0);
-        // Set fill color
-        gc.setFill(Color.RED);
-        // Draw a rounded Rectangle
-        gc.strokeRoundRect(10, 10, 50, 50, 10, 10);
-        // Draw a filled rounded Rectangle
-        gc.fillRoundRect(100, 10, 50, 50, 10, 10);
-        // Change the fill color
-        gc.setFill(Color.BLUE);     
-        // Draw an Oval
-        gc.strokeOval(10, 70, 50, 30);
-        // Draw a filled Oval
-        gc.fillOval(100, 70, 50, 30);
-        // Draw a Line
-        gc.strokeLine(200, 50, 300, 50);
-        // Draw an Arc
-        gc.strokeArc(320, 10, 50, 50, 40, 80, ArcType.ROUND);
-        // Draw a filled Arc
-        gc.fillArc(320, 70, 50, 50, 00, 120, ArcType.OPEN);
-         
-         
-        // Create the Pane
-        Pane root = new Pane();
-        // Set the Style-properties of the Pane
-        root.setStyle("-fx-padding: 10;" +
-                "-fx-border-style: solid inside;" +
-                "-fx-border-width: 2;" +
-                "-fx-border-insets: 5;" +
-                "-fx-border-radius: 5;" +
-                "-fx-border-color: blue;");
-         
-        // Add the Canvas to the Pane
-        root.getChildren().add(canvas);
-        // Create the Scene
-        Scene scene = new Scene(root);
-        // Add the Scene to the Stage
+		this.primaryStage= primaryStage;
+		this.primaryStage.setTitle("Drawing Operations Test");
+        this.root = new Group();
+        scene = new Scene(root, 600, 600);
         primaryStage.setScene(scene);
-        // Set the Title of the Stage
-        primaryStage.setTitle("Drawing Basewqetrewtewtic Shapes on a Canvas");
-        // Display the Stage
         primaryStage.show();
-        System.out.println("!");
+//        b1 = new Bohrer(this);
 	}
+	
+	void drawCircle(int x, int y) {
+		Circle r = new Circle();
+        r.setCenterX((float)x);
+        r.setCenterY((float)y);
+        r.setRadius(2.0f);
+
+        r.setFill(Color.ORANGE);
+        root.getChildren().add(r);
+        
+	    try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+   }
 }
+
