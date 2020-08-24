8e8f45b83c6efa9bf082125314c9ef15f1dc15a7
I516258 <I516258@WDFN34686032A.emea.global.corp.sap>
2020-08-24 15:00:26 +0200

    [Simon] annotated logger class

--- src/cnc_frase_testing/Logger.java
+++ src/cnc_frase_testing/Logger.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 
 import org.json.JSONArray;
 
+/**
+ * 
+ * Class responsible for writing log data to the file system
+ * 
+ * @author Tim
+ *
+ */
 public class Logger {
 	private static final Logger OBJ = new Logger();
 
@@ -12,6 +19,13 @@ public class Logger {
 		return OBJ;
 	}
 
+	/**
+	 * 
+	 * Stores the passed JSONArray in the file system
+	 * 
+	 * @param logArray
+	 * @author Tim
+	 */
 	public void logToFile(JSONArray logArray) {
 		try {
 			FileWriter file = new FileWriter("data//CNC_Fraese_Log.json");
