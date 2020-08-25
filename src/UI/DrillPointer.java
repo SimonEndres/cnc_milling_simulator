f07b3d0f7e676f9541db98a30c0e2a75521ee3cd
i517002 <jonas.haas@sap.com>
2020-08-25 14:00:53 +0200

    [jonas] added comments

--- src/UI/DrillPointer.java
+++ src/UI/DrillPointer.java
@@ -4,24 +4,43 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+/**
+ * Class for displaying drillpointer
+ * @author Jonas
+ *
+ */
 public class DrillPointer extends Canvas {
 
 	private GraphicsContext gc;
 
+	/**
+	 * Constructor for Drillpointer class
+	 * @param i
+	 * @param j
+	 */
 	public DrillPointer(int i, int j) {
 		super(i, j);
 		this.gc = this.getGraphicsContext2D();
+		gc.setFill(Color.RED);
 
 	}
-
+	/**
+	 * Method for displaying drillpointer on current position
+	 * @param x
+	 * @param y
+	 */
 	public void drawPoint(int x, int y) {
 
 		gc.clearRect(0, 0, 840, 630);
 		gc.fillOval(x, y, 4, 4);
-		gc.setFill(Color.RED);
 
 	}
 	
+	/**
+	 * Method to clear drillpointer
+	 * @param x
+	 * @param y
+	 */
 	public void clearAll() {
 		gc.clearRect(0, 0, this.getWidth(), this.getHeight());
 	}
--- src/UI/HomePoint.java
+++ src/UI/HomePoint.java
@@ -4,6 +4,11 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+/**
+ * Class for displaying HomePoint
+ * @author Jonas
+ *
+ */
 public class HomePoint extends Canvas {
 	
 	private GraphicsContext gc;
