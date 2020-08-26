06381adedb8d728e2586540c7164414abea952c5
i517002 <jonas.haas@sap.com>
2020-08-26 20:35:12 +0200

    [jonas] comments main

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -7,13 +7,25 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+/**
+ * Main Class of JavaFx Application. Responsible for Loading Fxml and UserInterface as well as creating UIController
+ * and showing UI on stage
+ * @author Tim, Simon, Jonas
+ *
+ */
 public class Main extends Application {
-	
-
+	/**
+	 * main method of project starts javafx application
+	 * @param args
+	 * @author Tim, Simon, Jonas
+	 */
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
-
+	/**
+	 * start method of javafx application. Creates UI and adds it to primarystage and creates Controller
+	 * @author Tim, Simon, Jonas
+	 */
 	public void start(Stage primaryStage) throws Exception {
 		FXMLLoader loader = new FXMLLoader(Main.class.getResource("UI.fxml"));
 		
