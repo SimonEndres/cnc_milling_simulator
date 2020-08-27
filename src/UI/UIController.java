c5f9598cd75548c5527b2d1aff8e43f4e06432b2
Tim Bleicher <tim.bleicher@sap.com>
2020-08-27 15:11:10 +0200

    [Tim] finished Settings Feature, added Commentary

--- src/UI/UIController.java
+++ src/UI/UIController.java
@@ -55,6 +55,7 @@ public class UIController {
 	private int speed;
 
 	private ObservableList<String> commandColl;
+	private ObservableList<String> settingsColl;
 	final FileChooser fileChooser = new FileChooser();
 
 	@FXML
@@ -92,7 +93,7 @@ public class UIController {
 	@FXML
 	private Label currPosition;
 	@FXML
-	private Button buttSettings;
+	private ComboBox<String> comboSett;
 	@FXML
 	private ColorPicker colorPic;
 
@@ -111,6 +112,7 @@ public class UIController {
 		this.speed = 4;
 		commandColl = FXCollections.observableArrayList("M00", "M02", "M03", "M04", "M05", "M08", "M09", "M13", "M14",
 				"G00", "G01", "G02", "G03", "G28");
+		settingsColl = FXCollections.observableArrayList("Drill", "Homepoint", "Work surface");
 	}
 
 	/**
@@ -131,6 +133,7 @@ public class UIController {
 		this.homePoint = homePoint;
 		this.workSurfaceGroup = workSurfaceGroup;
 		comboBox.setItems(commandColl);
+		comboSett.setItems(settingsColl);
 		tfX.textProperty().addListener((obs, oldText, newText) -> onInputChanged('X', newText));
 		tfY.textProperty().addListener((obs, oldText, newText) -> onInputChanged('Y', newText));
 		tfI.textProperty().addListener((obs, oldText, newText) -> onInputChanged('I', newText));
@@ -165,7 +168,7 @@ public class UIController {
 	}
 	
 	/**
-	 * Event method for choosing g-code in dropdown
+	 * Event method for choosing g-code in dropdown (For manual entry)
 	 * @param event
 	 * @author Tim
 	 */
@@ -197,6 +200,11 @@ public class UIController {
 		onInputChanged('p', "proof");
 	}
 
+	/**
+	 * Submit for manual entry - start process of checking and calculating command
+	 * @author Tim
+	 * @param event
+	 */
 	@FXML
 	void onPressSubmit(ActionEvent event) {
 		JSONObject newJson = new JSONObject();
@@ -228,7 +236,7 @@ public class UIController {
 	}
 	
 	/**
-	 * Event method for changing drillspeed
+	 * Event method for changing driving speed of the drill (when it isn't milling)
 	 * @param event
 	 * @author Tim
 	 */
@@ -297,7 +305,7 @@ public class UIController {
 	}
 	
 	/**
-	 * Resetting UI by User (Button)
+	 * Resetting UI by User (Button) - like changing the work surface
 	 * 
 	 * @author Tim
 	 * @param event
@@ -321,10 +329,28 @@ public class UIController {
 		setPosition("0.0 / 0.0");
 		showMessage("Worksurface successfully reset");
 	}
+	
+	
+	/**
+	 * Enable changeing color of Drill, Homepoint and work surface
+	 * @author Jonas, Tim
+	 * @param event
+	 */
+	@FXML
+	void onColorChange(ActionEvent event) {
+		String value = comboSett.getValue();
+		if (value.equals("Drill")) {
+			setDrillColor();
+		} else if (value.equals("Homepoint")) {
+			setHomeColor();
+		} else {
+			setWorkColor();
+		}
+	}
 	/**
 	 * Method to open Logfile
 	 * @param event
-	 * @author Tim
+	 * @author Simon
 	 */
 	@FXML
 	void onPressLog(ActionEvent event) {
@@ -338,6 +364,9 @@ public class UIController {
 		}
 	}
 	
+	/**Method triggered by process end. Enables Reset.
+	 * @author Tim
+	 */
 	public void millEnd() {
 		myThread.pause();
 		myThread.setRunning(false);
@@ -382,31 +411,24 @@ public class UIController {
 
 	/**
 	 * Method to format inputs only allowing integer values
-	 * @param field
-	 * @param newText
+	 * @param field		- defines changed inputfield
+	 * @param newText	- entered Text
 	 * @author Tim
 	 */
 	private void numberFormatter(char field, String newText) {
-		String value;
+		String value = newText;
+		value = value.replaceAll("\\b-?[^0-9]", "");
 		switch (field) {
 		case 'X':
-			value = tfX.getText();
-			value = value.replaceAll("\\b-?[^0-9]", "");
 			tfX.setText(value);
 			break;
 		case 'Y':
-			value = tfY.getText();
-			value = value.replaceAll("\\b-?[^0-9]", "");
 			tfY.setText(value);
 			break;
 		case 'I':
-			value = tfI.getText();
-			value = value.replaceAll("\\b-?[^0-9]", "");
 			tfI.setText(value);
 			break;
 		case 'J':
-			value = tfJ.getText();
-			value = value.replaceAll("\\b-?[^0-9]", "");
 			tfJ.setText(value);
 			break;
 		default:
@@ -415,8 +437,8 @@ public class UIController {
 	}
 
 	/**
-	 * Method to set commands which are toDo
-	 * @param text
+	 * Method to set commands that must be processed (To Do)
+	 * @param text		- entered Command (with parameters)
 	 * @author Tim
 	 */
 	public void setCommandsToDo(String text) {
@@ -425,8 +447,7 @@ public class UIController {
 	}
 	
 	/**
-	 * Method to update ToDocommands on Ui
-	 * @param text
+	 * Method to update To Do commands on UI
 	 * @author Tim
 	 */
 	public void updateCommandsToDo() {
@@ -436,8 +457,7 @@ public class UIController {
 		}
 	}
 	/**
-	 * Method to set Commands done also displaying time taken
-	 * @param text
+	 * Method to set commands done, also displaying runtime
 	 * @author Tim
 	 */
 	public void setCommandsDone() {
@@ -470,21 +490,35 @@ public class UIController {
 
 	/**
 	 * Method to get the speed of the drill (speed while not milling)
-	 * 
 	 * @author Tim
 	 */
 	public int getSpeed() {
 		return speed;
 	}
 	
+	/**
+	 * Update Drill Information spindle status
+	 * @author Tim
+	 * @param status	- spindle status
+	 */
 	public void setSpinStat(String status) {
 		spinStat.setText(status);
 	}
 
+	/**
+	 * Update Drill Information rotation direction
+	 * @author Tim
+	 * @param direction		- new rotation direction
+	 */
 	public void setRotDir(String direction) {
 		rotDir.setText(direction);
 	}
 
+	/**
+	 * Update Drill Information cooling status
+	 * @author Tim
+	 * @param status		- new cooling status
+	 */
 	public void setCoolStat(boolean status) {
 		if (status) {
 			coolStat.setFill(Color.SKYBLUE);
@@ -493,23 +527,35 @@ public class UIController {
 		}
 	}
 	
+	/**
+	 * Update Drill Information current position (as coordinates)
+	 * @author Tim
+	 * @param newPos		- new current position 
+	 */
 	public void setPosition(String newPos) {
 		currPosition.setText(newPos);
 	}
 	
-	@FXML
-	public void setHomeColor(ActionEvent event) {
-		System.out.println(colorPic.getValue());
+	/**
+	 * Changes Color of Homepoint
+	 * @author Jonas, Tim
+	 */
+	public void setHomeColor() {
 		homePoint.setColor(colorPic.getValue());
-		setWorkColor();
 	}
 	
+	/**
+	 * Changes Color of Work surface
+	 * @author Jonas, Tim
+	 */
 	public void setWorkColor() {
-		System.out.println();
 		workSurfaceGroup.setStyle("-fx-background-color: " + colorPic.getValue().toString().replaceAll("0x", "#"));
-		setDrillColor();
 	}
 	
+	/**
+	 * Changes Color of Drill
+	 * @author Jonas, Tim
+	 */
 	public void setDrillColor() {
 		drillPointer.setColor(colorPic.getValue());
 	}
