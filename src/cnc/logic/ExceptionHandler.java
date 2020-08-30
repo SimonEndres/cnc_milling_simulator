8283f5d1aeb851a3ccb52995de6b62b93d7a4d43
I516258 <I516258@WDFN34686032A.emea.global.corp.sap>
2020-08-30 20:53:26 +0200

    [Simon] annotated exception handler

--- src/cnc/logic/ExceptionHandler.java
+++ src/cnc/logic/ExceptionHandler.java
@@ -3,22 +3,53 @@ package cnc.logic;
 import cnc.ui.UIController;
 /**
  * 
- * Class that includes handling of occured errors.
+ * Class that includes handling of occured errors. All Errors get logged automatically.
  * 
  * @author Tim, Jonas, Simon
  *
  */
 public class ExceptionHandler{
 
+	/**
+	 * 
+	 * Method to handle Errors by terminating process
+	 * 
+	 * @author Tim, Simon
+	 * @param ui
+	 * @param cp
+	 * @param reason
+	 * @param handling
+	 */
 	public static void handleErrorByTerminating(UIController ui,CommandProcessor cp, String reason, String handling) {
 		logError(cp, reason, handling);
 		ui.terminate();
 		ui.showError(reason + "- Handling: " + handling);
 	}
+	
+	/**
+	 * 
+	 * Method to handle Errors showing a message to the user
+	 * 
+	 * @author Tim, Simon
+	 * @param ui
+	 * @param cp
+	 * @param reason
+	 * @param handling
+	 */
 	public static void handleErrorByMessage(UIController ui,CommandProcessor cp,String reason, String handling) {
 		logError(cp, reason, handling);
 		ui.showError(reason + "- Handling: " + handling);
 	}
+	
+	/**
+	 * 
+	 * Method to log errors.
+	 * 
+	 * @author Tim
+	 * @param cp
+	 * @param reason
+	 * @param handling
+	 */
 	public static void logError(CommandProcessor cp, String reason, String handling) {
 		cp.logMessage("ERROR", reason, handling);
 	}
